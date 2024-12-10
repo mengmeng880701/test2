c991cad77d24cc58798f7eba70215bf48c44bab9
mengchien <pk6507223@gmail.com>
2024-12-10 22:56:21 +0800

    test2

--- Repository/Database/Major.cs
+++ Repository/Database/Major.cs
@@ -1,5 +1,4 @@
-﻿using NPOI.SS.Formula.Functions;
-using Repository.Bases;
+﻿using Repository.Bases;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +7,9 @@ using System.Threading.Tasks;
 
 namespace Repository.Database
 {
-    public class TTeacher : CD
+    public class Major:CD
     {
         public string Name { get; set; }
-
-        public string phone {  get; set; }
-
-        public string? Email { get; set; }
 
         public string? Description { get; set; }
 
